e166dd6b7f06d77435337ebe0ed7c4220a278e1a
xiu14 <ouhao1992@gmail.com>
2026-02-12 04:10:22 +0800

    fix: bump version to v64

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* MiniPhone Service Worker - Cache-first for static assets */
-const CACHE_NAME = 'miniphone-v63';
+const CACHE_NAME = 'miniphone-v64';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
